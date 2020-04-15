48e96873edb612b96ad4e1b7e9702e87cda98c7b
Huang Yin <1071324110@qq.com>
2020-04-15 23:58:31 +0800

    fix bug

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def train_from_folder(data='../../gan/custom_dataset',
 
     model.set_data_src(data)
 
-    train_now = datetime.now()
+    train_now = datetime.now().timestamp()
     for _ in tqdm(range(num_train_steps - model.steps), mininterval=10., desc=f'{name}<{data}>'):
         #train
         retry_call(model.train, tries=3, exceptions=NanException)
