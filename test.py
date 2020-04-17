201f26e520bca3227a5ce3d637d5356d2200cd83
Huang Yin <1071324110@qq.com>
2020-04-18 01:36:46 +0800

    test

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import torch
 from utils.utils import NanException
 
+import numpy as np
+
+
 if __name__ == "__main__":
-    a = torch.Tensor(3,)
-    raise NanException
-    print(a)
+    init_dim = 8
+    n_tile = 8
+    t_l = [init_dim * np.arange(n_tile) + i for i in range(init_dim)]
+    print(t_l)
